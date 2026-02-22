98b35fab4a7758c754723e9a94f3eb5c0ef64cf3
Bob Long <robertlong13@gmail.com>
2026-02-22 12:58:24 +1100

    Python: add uv tooling

--- scripts/regenerate-mavlink.py
+++ scripts/regenerate-mavlink.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python3
-# Groundwork
-# Copyright (C) 2026 Bob Long
-#
 # SPDX-License-Identifier: GPL-3.0-or-later
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
 
 """Regenerate MAVLink C# bindings from pymavlink.
 
