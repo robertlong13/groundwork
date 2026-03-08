8d0713b6994c7d1a4d66e9a12a991070c76e2ba7
Bob Long <robertlong13@gmail.com>
2026-03-08 23:01:34 +1100

    Params: add file I/O and filtering

---
+++ src/Groundwork.Core/ArduPilot/ParamLoadExclusions.cs
@@ -0,0 +1,29 @@
+// Groundwork
+// Copyright (C) 2026 Bob Long
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+namespace Groundwork.Core.ArduPilot;
+
+/// <summary>
+/// Provides ArduPilot parameters that should be excluded when loading from
+/// a .parm file because they sneakily cause problems.
+/// </summary>
+/// <remarks>
+/// These parameters should be marked in some way in ArduPilot metadata (@ReadOnly, even if they
+/// technically can be written to) but are not. If AP ever fixes their metadata, this class becomes
+/// empty.
+/// </remarks>
+public static class ParamLoadExclusions
+{
+    /// <summary>
+    /// Gets the default exclusion patterns for file loads.
+    /// </summary>
+    public static IReadOnlySet<string> DefaultWildcards { get; } =
+        new HashSet<string>(StringComparer.OrdinalIgnoreCase) { "FENCE_TOTAL", "FORMAT_VERSION" };
+}
